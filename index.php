51d4edd52f78a08f12e7eb034ee91f10176c77ba
RandsTaggle <cyril.lemaire@wanadoo.fr>
2016-09-08 11:20:41 +0200

    Added Materialize and JQuery CDN to index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+	<link ref="alternative stylesheet" href="css/materialize.min.css"/>
+	<title>Atelier Vue Modulaire</title>
+</head>
+<body>
+	
+	<!-- JQuery -->
+	<script
+		src="https://code.jquery.com/jquery-3.1.0.min.js"
+		integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="
+		crossorigin="anonymous"/>
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+</body>
+</html>
+
